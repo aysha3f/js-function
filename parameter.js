d8c1f67bb8334d6b4a6a5d7704b376ca3e0219c9
aysha3f <tahiaysha220@gmail.com>
2025-03-26 02:16:19 +0600

    basic of parameter

---
+++ parameter.js
@@ -0,0 +1,41 @@
+function vaatKhao(){
+    console.log('hand wash kore aso');
+    console.log('bosho');
+    console.log('plate e khabar dhavar nao');
+    console.log('gopat gopat kore khao')
+
+}
+// vaatKhao();
+
+
+// 3 --> 3*3 = 9
+// 4 --> 4*4 = 16
+// 5 --> 5*5 = 25
+// 11 --> 11*11 = 121
+
+function square (number){
+console.log(number);
+console.log('Value of the number parameter', number)
+const borgo = number * number;
+console.log('Square of the given number is:', borgo);
+}
+
+square(4);
+square(12);
+square(512);
+square(5);
+square(405);
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    console.log(sum);
+}
+
+add(10, 23);
+add(65, 31);
+
+function addAll(a,b,c,d,e){
+    const total = a + b + c + d + e;
+    console.log(total);
+}
+addAll(3,4,5,5,5)
